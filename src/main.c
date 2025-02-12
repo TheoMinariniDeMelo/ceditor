5a4d302302ee35133509d719fa5181e622dde364
TheoMinariniDeMelo <theominarini@outlook.com>
2025-02-12 14:10:53 -0300

    Initial project Add Terminal resize & signal handler Initial open file system

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include "./tty.c"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char** argv){
+	resize_handler(0);
+	signal(SIGWINCH, resize_handler);
+
+	if(strlen(argv[0]) == 0) return 1;
+	
+	char *file_name = argv[0];
+
+	FILE* file = fopen(file_name, "rw");
+
+	if(file == NULL) return 1;
+	
+	unsigned int BUFFER_SIZE = max_col * max_line;
+	char *buffer = (char*) malloc(BUFFER_SIZE * sizeof(char));
+	
+	if(fgets(buffer, BUFFER_SIZE, file) == NULL){
+		free(buffer);
+	} 
+}
---
+++ src/tty.c
@@ -0,0 +1,15 @@
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+unsigned int max_line;
+unsigned int max_col;
+
+void resize_handler(int sig){
+	struct winsize ws;	
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) {
+	}
+	max_line = ws.ws_row;  // Atualiza o número de linhas
+	max_col = ws.ws_col;   // Atualiza o número de colunas
+}
